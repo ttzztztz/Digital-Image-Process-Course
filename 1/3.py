bb9d603a6006e005bc9aee7ac3638c798401844e
ttzztztz <ttzztztz@outlook.com>
2020-11-29 15:21:48 +0800

    fix

--- 1/3.py
+++ 1/3.py
@@ -15,3 +15,5 @@ cv2.polylines(img, [pts], True, (0, 255, 255))
 linetype = cv2.LINE_AA
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2)
+
+cv2.imwrite('test2.jpg', img)
